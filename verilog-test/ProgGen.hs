3ec86033965e2bce8259285067bac7419bd15ec2
Adam Walker <adamwalker10@gmail.com>
2017-01-18 21:45:10 +1100

    test recursive fib

--- verilog-test/ProgGen.hs
+++ verilog-test/ProgGen.hs
@@ -2,8 +2,8 @@ import Data.Word
 import RiscV.RV32I
 import RiscV.Encode.RV32I
 
-program :: [Instr]
-program = [
+fib2 :: [Instr]
+fib2 = [
         --Faster fibbonnacci
         --Initialize the first 2 fibbonnacci numbers - in X5 and X6
         RIInstr     $ IInstr ADDI (Word12 0) X0 X5,
@@ -17,10 +17,85 @@ program = [
         RIInstr     $ IInstr ADDI (Word12 (-1)) X3 X3,
         --Branch 
         BranchInstr $ Branch (Word12 (-6)) BNE X3 X0,
+        --Write the output
         MemoryInstr $ STORE  Word (Word12 (-12)) X6 X0
     ]
 
+--A Naiive, recursive, exponential time fibbonnacci function
+recursiveFib :: [Instr]
+recursiveFib = [
+        --Calling convention: 
+        -- The caller saves all registers it needs. The stack pointer is assumed unclobbered.
+        -- -Stack pointer: X1
+        -- -Single argument in X2
+        -- -Return address in X3
+        -- -Return value in X4
+        
+        --Make the function call
+        --Setup the stack pointer. The stack grows upwards.
+        RIInstr     $ IInstr ADDI (Word12 0) X0 X1,
+        --Load the fib index into register 2. This is the function argument.
+        RIInstr     $ IInstr ADDI (Word12 8) X0 X2,
+        --Call the function, X3 contains the return address
+        JumpInstr   $ JAL    (Word20 6) X3,
+
+        --Write the output in a loop
+        MemoryInstr $ STORE  Word (Word12 (-12)) X4 X0,
+        JumpInstr   $ JAL    (Word20 (-2)) X0,
+        
+        --The function
+        --We need to make a recursive call, so we need to store our local vars in a stack frame
+        --Layout:
+        -- -Function argument
+        -- -Return address
+        -- -Single local var (the return value of the first recursive fib call)
+
+        -- The base cases
+        -- 0
+        -- PC: 4
+        BranchInstr $ Branch (Word12 6) BNE X0 X2,
+        RIInstr     $ IInstr ADDI (Word12 0) X0 X4,
+        JumpInstr   $ JALR   (Word12 0)  X3 X0 ,
+
+        -- 1
+        -- PC: 7
+        RIInstr     $ IInstr ADDI (Word12 (-1)) X2 X6,
+        BranchInstr $ Branch (Word12 6) BNE X0 X6,
+        RIInstr     $ IInstr ADDI (Word12 1) X0 X4,
+        JumpInstr   $ JALR   (Word12 0)  X3 X0 ,
+
+        -- PC:11
+        --Adjust the stack pointer to make space for our local variables
+        RIInstr     $ IInstr ADDI (Word12 12) X1 X1,
+        --Save the argument and return address
+        MemoryInstr $ STORE  Word (Word12 (-12)) X2 X1,
+        MemoryInstr $ STORE  Word (Word12 (-8))  X3 X1,
+        --Set the argument for the first recursive call
+        RIInstr     $ IInstr ADDI (Word12 (-1)) X2 X2,
+        --Make the first recursive call
+        JumpInstr   $ JAL    (Word20 (-22)) X3,
+        --Copy the return value to its place on the stack
+        MemoryInstr $ STORE  Word (Word12 (-4)) X4 X1,
+        --Restore the argument to this function
+        MemoryInstr $ LOAD   (Width Word) (Word12 (-12)) X1 X2,
+        --Set the argument for the second recursive call
+        RIInstr     $ IInstr ADDI (Word12 (-2)) X2 X2,
+        --Make the second recursive call
+        JumpInstr   $ JAL    (Word20 (-30)) X3,
+        --Load the saved result from the first recursive call
+        MemoryInstr $ LOAD   (Width Word) (Word12 (-4)) X1 X5,
+        --Add the results of the recursive call and put it in the result register
+        RRInstr     $ RInstr ADD X4 X5 X4,
+        --Restore the saved return address
+        MemoryInstr $ LOAD   (Width Word) (Word12 (-8)) X1 X3,
+        --Put the stack pointer back
+        RIInstr     $ IInstr ADDI (Word12 (-12)) X1 X1,
+        --Return
+        JumpInstr   $ JALR   (Word12 0)  X3 X0 
+
+    ]
+
 assembled :: [Word32]
-assembled = map encodeInstr program
+assembled = map encodeInstr recursiveFib
 
 main = print assembled
--- verilog-test/test.v
+++ verilog-test/test.v
@@ -21,23 +21,66 @@ module main;
     //instruction memory
     reg [31:0] instructionMem [255:0];
 
+    //initialise the instruction memory
     initial begin
-        instructionMem[0] = 659;
-        instructionMem[1] = 1049363;
-        instructionMem[2] = 5243283;
-        instructionMem[3] = 5440179;
-        instructionMem[4] = 5440307;
-        instructionMem[5] = 4294017427;
-        instructionMem[6] = 4264565475;
-        instructionMem[7] = 4267715107;
+        //non-recursive fib
+        //instructionMem[0] = 659;
+        //instructionMem[1] = 1049363;
+        //instructionMem[2] = 5243283;
+        //instructionMem[3] = 5440179;
+        //instructionMem[4] = 5440307;
+        //instructionMem[5] = 4294017427;
+        //instructionMem[6] = 4264565475;
+        //instructionMem[7] = 4267715107;
+        
+        //recursive fib
+        instructionMem[0]  = 147;
+        instructionMem[1]  = 8388883;
+        instructionMem[2]  = 12583407;
+        instructionMem[3]  = 4265617955;
+        instructionMem[4]  = 4292866159;
+        instructionMem[5]  = 71267;
+        instructionMem[6]  = 531;
+        instructionMem[7]  = 98407;
+        instructionMem[8]  = 4293985043;
+        instructionMem[9]  = 202339;
+        instructionMem[10]  = 1049107;
+        instructionMem[11] = 98407;
+        instructionMem[12] = 12615827;
+        instructionMem[13] = 4263553571;
+        instructionMem[14] = 4264602659;
+        instructionMem[15] = 4293984531;
+        instructionMem[16] = 4250923503;
+        instructionMem[17] = 4265651747;
+        instructionMem[18] = 4282425603;
+        instructionMem[19] = 4292935955;
+        instructionMem[20] = 4234146287;
+        instructionMem[21] = 4290814595;
+        instructionMem[22] = 4358707;
+        instructionMem[23] = 4286620035;
+        instructionMem[24] = 4282417299;
+        instructionMem[25] = 98407;
+    end
+
+    //read the instruction memory
+    always @ (posedge clk) begin
+        instruction = instructionMem[instructionAddress];
     end
 
     //data memory
     reg [31:0] dataMem [65535:0];
 
+    //read/write the data memory
     always @ (posedge clk) begin
-        instruction = instructionMem[instructionAddress];
+
+        memoryData = dataMem[readAddress];
+
+        if (writeStrobe != 0) begin
+            dataMem[writeAddress] = writeData;
+        end
+
     end
+
 
     //generate the clock
     always #1 clk = ~clk;
@@ -77,7 +120,7 @@ module main;
     //run for 1000 cycles
     initial
     begin
-        #1000;
+        #2500;
         $finish;
     end
 
