d3b279607abb5ea706d5d91ccf6a99dea050e6f9
Adam Walker <adamwalker10@gmail.com>
2017-01-24 18:38:00 +1100

    regfile bypassing

--- core/RegFile.hs
+++ core/RegFile.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DataKinds, NoImplicitPrelude #-}
-module RegFile where
+module RegFile (regFile) where
 
 import CLaSH.Prelude
 
@@ -12,16 +12,21 @@ regFile
     -> Signal Bool           --Write enable
     -> Signal (BitVector 32) --Write data
     -> (Signal (BitVector 32), Signal (BitVector 32), Signal (Vec 32 (BitVector 32)))
-regFile rs1 rs2 writeAddr writeEn writeData = (readReg <$> file <*> rs1, readReg <$> file <*> rs2, file)
+regFile rs1 rs2 writeAddr writeEn writeData = (bypass <$> file <*> rs1 <*> writeEn <*> writeAddr <*> writeData, bypass <$> file <*> rs2 <*> writeEn <*> writeAddr <*> writeData, file)
     where
     file = mealy step (repeat 0) $ bundle (writeAddr, writeEn, writeData)
         where
         step :: RegFile -> (Index 32, Bool, BitVector 32) -> (RegFile, RegFile)
-        step regFile (writeAddr, writeEn, writeData) = (regFile', regFile')
+        step regFile (writeAddr, writeEn, writeData) = (regFile', regFile)
             where
             regFile'
                 | writeEn   = replace writeAddr writeData regFile
                 | otherwise = regFile
+
+bypass :: Vec 32 (BitVector 32) -> Index 32 -> Bool -> Index 32 -> BitVector 32 -> BitVector 32
+bypass file readAddr writeEn writeAddr writeData 
+    | readAddr == writeAddr && writeEn = writeData
+    | otherwise                        = readReg file readAddr
             
 readReg :: Vec 32 (BitVector 32) -> Index 32 -> BitVector 32
 readReg regFile idx
