550024226e03385a8f0d8aaa4ddb5097abf6d068
Adam Walker <adamwalker10@gmail.com>
2017-10-03 14:03:25 +1100

    replacement comments

--- src/Cache/Replacement.hs
+++ src/Cache/Replacement.hs
@@ -7,7 +7,11 @@ module Cache.Replacement where
 
 import CLaSH.Prelude
 
-type ReplacementFunc indexBits ways = Signal (BitVector indexBits) -> Signal Bool -> Signal (Index ways) -> Signal (Index ways)
+type ReplacementFunc indexBits ways 
+    =  Signal (BitVector indexBits) --Index being looked up in the current cycle
+    -> Signal Bool                  --Hit in next cycle
+    -> Signal (Index ways)          --Way that was hit (if any) in the next cycle
+    -> Signal (Index ways)          --Way to replace in the current cycle
 
 randomReplacement :: ReplacementFunc indexBits 2
 randomReplacement _ _ _ = bitCoerce <$> toReplace
