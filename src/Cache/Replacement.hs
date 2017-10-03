0e8fcbdf051843954f6bd99e0264f24471aa833a
Adam Walker <adamwalker10@gmail.com>
2017-10-03 18:25:44 +1100

    pseudoLRUReplacement

--- src/Cache/Replacement.hs
+++ src/Cache/Replacement.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, ScopedTypeVariables, TypeOperators, NoImplicitPrelude, GADTs #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Extra.Solver #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
@@ -6,6 +6,8 @@
 module Cache.Replacement where
 
 import CLaSH.Prelude
+
+import Cache.PseudoLRUTree
 
 type ReplacementFunc indexBits ways 
     =  Signal (BitVector indexBits) --Index being looked up in the current cycle
@@ -17,4 +19,19 @@ randomReplacement :: ReplacementFunc indexBits 2
 randomReplacement _ _ _ = bitCoerce <$> toReplace
     where
     toReplace = register False $ not <$> toReplace
+
+pseudoLRUReplacement 
+    :: forall indexBits numWaysLog. (KnownNat indexBits, KnownNat numWaysLog, 1 <= numWaysLog) 
+    => ReplacementFunc indexBits (2 ^ numWaysLog)
+pseudoLRUReplacement index valid way = bitCoerce . getOldestWay <$> readResult
+    where
+    readResult :: Signal (Vec ((2 ^ numWaysLog) - 1) Bool)
+    readResult = readNew (blockRamPow2 (repeat (repeat False))) (unpack <$> index) write
+
+    lastIdx    = register 0 index
+
+    write      :: Signal (Maybe (Unsigned indexBits, Vec ((2 ^ numWaysLog) - 1) Bool))
+    write      = mux valid (func <$> index <*> readResult <*> way) (pure Nothing)
+        where 
+        func index readResult way = Just (unpack index, updateWay (bitCoerce way) readResult)
 
