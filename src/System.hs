533713d4d1bfb6f5e6ccec041aed241e84af764b
Adam Walker <adamwalker10@gmail.com>
2017-11-02 19:41:25 +1100

    make top level entities type correct

--- src/System.hs
+++ src/System.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE ImplicitParams #-}
 module System where
 
-import CLaSH.Prelude
+import Clash.Prelude
 import qualified Prelude as P
 import RiscV.RV32I
 import RiscV.Encode.RV32I
 import Core.Pipeline
 import Prog
 
-system :: Vec (2 ^ 10) (BitVector 32) -> Signal ToDataMem
+system :: HasClockReset dom sync gated => Vec (2 ^ 10) (BitVector 32) -> Signal dom ToDataMem
 system program = register (errorX "X") toDataMem 
     where
     --The instruction memory
@@ -22,8 +23,10 @@ system program = register (errorX "X") toDataMem
 
 {-# ANN topEntity
   (defTop
-    { t_name     = "riscvPipeline"
-    , t_outputs  = ["readAddress", "writeAddress", "writeData", "writeStrobe"]
+    { t_name   = "riscvPipeline"
+    , t_inputs = [PortName "clk", PortName "rst"]
+    , t_output = PortField "res" [PortName "readAddress", PortName "writeAddress", PortName "writeData", PortName "writeStrobe"]
     }) #-}
+topEntity :: Clock System Source -> Reset System Synchronous -> Signal System ToDataMem
+topEntity clk rst = withClockReset clk rst $ system ($(listToVecTH (P.map encodeInstr fib)) ++ repeat 0)
 
-topEntity = system ($(listToVecTH (P.map encodeInstr fib)) ++ repeat 0)
--- src/SystemWithCache.hs
+++ src/SystemWithCache.hs
@@ -1,25 +1,26 @@
 module System where
 
-import CLaSH.Prelude
+import Clash.Prelude
 import qualified Prelude as P
 import RiscV.RV32I
 import RiscV.Encode.RV32I
 import Core.Pipeline
 import Prog
 import Cache.ICache
+import Cache.Replacement
 
-systemWithCache :: Vec (2 ^ 10) (BitVector 32) -> Signal Bool -> Signal ToDataMem
+systemWithCache :: forall dom sync gated. HasClockReset dom sync gated => Vec (2 ^ 10) (BitVector 32) -> Signal dom Bool -> Signal dom ToDataMem
 systemWithCache program instrStall = toDataMem
     where
     lines :: Vec (2 ^ 6) (Vec 16 (BitVector 32))
     lines = unconcatI program
 
     --The instruction memory
-    fromMem :: Signal (Vec 16 (BitVector 32))
+    fromMem :: Signal dom (Vec 16 (BitVector 32))
     fromMem = romPow2 lines ((unpack . resize) <$> memAddr)
 
     --The instruction cache
-    (instrReady, instrData, memReq, memAddr) = iCache (SNat @ 20) (SNat @ 6) (pure True) ((pack . instructionAddress) <$> toInstructionMem) (pure True) fromMem
+    (instrReady, instrData, memReq, memAddr) = iCache (SNat @ 20) (SNat @ 6) (SNat @ 4) pseudoLRUReplacement (pure True) ((pack . instructionAddress) <$> toInstructionMem) (pure True) fromMem
 
     --The data memory
     memReadData_3' = readNew (blockRamPow2 (repeat 0 :: Vec (2 ^ 10) (BitVector 32)))
@@ -31,8 +32,9 @@ systemWithCache program instrStall = toDataMem
 
 {-# ANN topEntity
   (defTop
-    { t_name     = "riscvPipeline"
-    , t_outputs  = ["readAddress", "writeAddress", "writeData", "writeStrobe"]
+    { t_name   = "riscvPipeline"
+    , t_inputs = [PortName "clk", PortName "rst"]
+    , t_output = PortField "res" [PortName "readAddress", PortName "writeAddress", PortName "writeData", PortName "writeStrobe"]
     }) #-}
 
-topEntity = systemWithCache ($(listToVecTH (P.map encodeInstr fib)) ++ repeat 0)
+topEntity clk rst = withClockReset clk rst $ systemWithCache ($(listToVecTH (P.map encodeInstr fib)) ++ repeat 0)
